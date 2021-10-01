33788bea0d622317f65edc53f33be8e6028e3931
Kasthoori <anurapkasthoori@gmail.com>
2021-10-01 16:56:40 +1300

    added routes

--- ang-express-crud/src/app/app-routing.module.ts
+++ ang-express-crud/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { BooksListComponent } from './components/books-list/books-list.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'add-book'},
+  {path: 'books-list', component: BooksListComponent},
+  {path: 'add-book', component: AddBookComponent},
+  {path: 'edit-book/:id', component: BookDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
